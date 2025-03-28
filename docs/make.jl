6ad26d5835f43320ffe0e162b7d08df139a7b6a8
Matt Fishman <mtfishman@users.noreply.github.com>
2025-03-28 10:17:50 -0400

    Update template (#40)

--- docs/make.jl
+++ docs/make.jl
@@ -15,9 +15,9 @@ makedocs(;
   authors="ITensor developers <support@itensor.org> and contributors",
   sitename="QuantumOperatorDefinitions.jl",
   format=Documenter.HTML(;
-    canonical="https://ITensor.github.io/QuantumOperatorDefinitions.jl",
+    canonical="https://itensor.github.io/QuantumOperatorDefinitions.jl",
     edit_link="main",
-    assets=String[],
+    assets=["assets/favicon.ico", "assets/extras.css"],
   ),
   pages=["Home" => "index.md", "Reference" => "reference.md"],
 )
