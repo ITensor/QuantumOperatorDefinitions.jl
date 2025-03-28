4c622e1830c74285fe7d4afca628d36ad4a68b57
mtfishman <mfishman@flatironinstitute.org>
2025-03-28 10:01:37 -0400

    Update template

--- docs/make.jl
+++ docs/make.jl
@@ -2,10 +2,7 @@ using QuantumOperatorDefinitions: QuantumOperatorDefinitions
 using Documenter: Documenter, DocMeta, deploydocs, makedocs
 
 DocMeta.setdocmeta!(
-  QuantumOperatorDefinitions,
-  :DocTestSetup,
-  :(using QuantumOperatorDefinitions);
-  recursive=true,
+  QuantumOperatorDefinitions, :DocTestSetup, :(using QuantumOperatorDefinitions); recursive=true
 )
 
 include("make_index.jl")
@@ -15,15 +12,13 @@ makedocs(;
   authors="ITensor developers <support@itensor.org> and contributors",
   sitename="QuantumOperatorDefinitions.jl",
   format=Documenter.HTML(;
-    canonical="https://ITensor.github.io/QuantumOperatorDefinitions.jl",
+    canonical="https://itensor.github.io/QuantumOperatorDefinitions.jl",
     edit_link="main",
-    assets=String[],
+    assets=["assets/favicon.ico", "assets/extras.css"],
   ),
   pages=["Home" => "index.md", "Reference" => "reference.md"],
 )
 
 deploydocs(;
-  repo="github.com/ITensor/QuantumOperatorDefinitions.jl",
-  devbranch="main",
-  push_preview=true,
+  repo="github.com/ITensor/QuantumOperatorDefinitions.jl", devbranch="main", push_preview=true
 )
